55075b5a6c9a0ed7b26ded341c2e19ca52d464ea
AvalonLucero <avalonlm15@gmail.com>
2025-01-12 19:32:31 -0600

    Start
    Simple start that is basic with links

--- script.js
+++
@@ -1,8 +0,0 @@
-// Theme Toggle
-const toggleThemeButton = document.getElementById("toggle-theme");
-toggleThemeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  toggleThemeButton.textContent = document.body.classList.contains("dark")
-    ? "☀️"
-    : "🌙";
-});
